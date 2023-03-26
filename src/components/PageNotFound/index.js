89224fd4ea2a374c05614dc90819d107197957ea
shurikperunov@mail.ru <shurikperunov@mail.ru>
2023-03-26 16:04:24 +0500

    Кнопка назад теперь работает

--- src/components/PageNotFound/index.js
+++ src/components/PageNotFound/index.js
@@ -1,13 +1,17 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import './index.css';
 
 const PageNotFound = () => {
+  const navigate = useNavigate();
+  const handleClick = () => navigate('/movies');
+
   return (
     <section className='page-not-found'>
       <div className='page-not-found__content'>
         <h2 className='page-not-found__title'>404</h2>
         <p className='page-not-found__subtitle'>Страница не найдена</p>
-        <a className='link page-not-found__link' href='#'>Назад</a>
+        <button className='btn page-not-found__link' onClick={handleClick}>Назад</button>
       </div>
     </section>
   );
